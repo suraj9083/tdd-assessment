d87459809d29b5dc1e7df0df6dbed9d5bba107db
suraj <surajsonkar1919@gmail.com>
2025-03-26 21:07:17 +0530

    Added extra test cases

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -16,4 +16,16 @@ describe('String Calculator', () => {
     test('should return the sum of multiple numbers', () => {
         expect(add('1,2,3,4,5')).toBe(15);
     });
+
+    test('should support newline as a delimiter', () => {
+        expect(add('1\n2,3')).toBe(6);
+    });
+
+    test('should handle multiple numbers and newlines', () => {
+        expect(add('1\n2,3\n4')).toBe(10);
+    });
+
+    test('should throw an error when a negative number is provided', () => {
+        expect(() => add('1,-2,3,-5')).toThrowError('Negative numbers not allowed');
+    });
 });
