3fb558895333111e15841bd412c2f3913b648d02
suraj <surajsonkar1919@gmail.com>
2025-03-26 21:06:05 +0530

    Added unit test case for simple cases

---
+++ test/calculator.test.js
@@ -0,0 +1,19 @@
+const { add } = require('../calculator');
+
+describe('String Calculator', () => {
+    test('should return 0 for an empty string', () => {
+        expect(add('')).toBe(0);
+    });
+
+    test('should return the number itself for a single number', () => {
+        expect(add('1')).toBe(1);
+    });
+
+    test('should return the sum of two numbers', () => {
+        expect(add('10,20')).toBe(30);
+    });
+
+    test('should return the sum of multiple numbers', () => {
+        expect(add('1,2,3,4,5')).toBe(15);
+    });
+});
